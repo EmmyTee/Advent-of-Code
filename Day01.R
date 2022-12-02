f89b64237fa5dc0ceb6fc033120721bc91097b1f
Yiu Sum Yip <24293400+EmmyTee@users.noreply.github.com>
2022-12-02 12:12:16 +0000

    Add files via upload

---
+++ Day01.R
@@ -0,0 +1,23 @@
+input = read.csv(
+  "InputDay1.csv",
+  header = FALSE,
+  blank.lines.skip = FALSE
+)
+
+output = data.frame(header = FALSE)
+count = 0
+
+for (i in 1:nrow(input)) {
+  if (!is.na(input[i,])) {
+    count = count + input[i,]
+  } else {
+    output = rbind(output, count)
+    count = 0
+  }
+}
+
+# Part 1
+max(output)
+
+# Part 2
+sum(sort(output[,1], decreasing = TRUE)[1:3])
